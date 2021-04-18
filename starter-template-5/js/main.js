288f90bde8bf655ffcc4287e8cf5bc9c45fc3c76
unuefeejovwoke <vuwill7114@gmail.com>
2021-04-18 05:32:37 -0700

    made corrections

---
+++ starter-template-5/js/main.js
@@ -0,0 +1,11 @@
+
+ /*window.addEventListener("load", () =>{
+    //preloader
+    document.querySelector(".preloader").classList.add("fade-out");
+    setTimeout(() =>{
+      document.querySelector(".preloader").style.display="none";
+    },3000)
+   })*/
+
+
+  
